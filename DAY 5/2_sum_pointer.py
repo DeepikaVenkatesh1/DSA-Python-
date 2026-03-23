6c286532a01d894896ed37acbe4922ddd6eba14a
Deepika V <deepikavenkatesh109@gmail.com>
2026-03-23 20:59:34 +0530

    Day 5 pointerTwoSum,StocksGreedy,Max Sub Array

---
+++ DAY 5/2_sum_pointer.py
@@ -0,0 +1,21 @@
+from typing import List
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            current_sum = numbers[left] + numbers[right]
+
+            if current_sum == target:
+                return [left+1, right+1]
+            elif current_sum < target:
+                left += 1
+            else:
+                right -= 1
+sol=Solution()
+print(sol.twoSum([1,2,3,4,5],7))
+
+
+
+
