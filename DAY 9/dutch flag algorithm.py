5697cd8ca44e74829ad65acc1f5b8e2fd67c1710
Deepika V <deepikavenkatesh109@gmail.com>
2026-03-27 20:29:39 +0530

    dutch algprithm

---
+++ DAY 9/dutch flag algorithm.py
@@ -0,0 +1,23 @@
+class Solution:
+    from typing import List
+    def dutchalgo(self,nums:List[int])->None:
+        low=0
+        mid=0
+        high=len(nums)-1
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low],nums[mid]=nums[mid],nums[low]
+                low+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
+        return nums
+sol=Solution()
+print(sol.dutchalgo([1,0,2,1,0,2]))
+
+    
+
+
