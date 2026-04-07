a51d1a9c8fbe83c9add8c9242f0fa922a1ffa64c
Deepika V <deepikavenkatesh109@gmail.com>
2026-04-07 20:16:35 +0530

    permutations

---
+++ DAY 20/permutations.py
@@ -0,0 +1,23 @@
+class Solution:
+    def permute(self, nums: list) -> list:
+        result = []
+
+        def backtrack(current, remaining):
+            if not remaining:           # no more elements!
+                result.append(current[:])
+                return
+
+            for i in range(len(remaining)):
+                current.append(remaining[i])
+                backtrack(current,
+                         remaining[:i] + remaining[i+1:])
+                current.pop()           # backtrack!
+
+        backtrack([], nums)
+        return result
+
+# Testing
+sol = Solution()
+print(sol.permute([1,2,3]))
+
+print(sol.permute([0,1]))
