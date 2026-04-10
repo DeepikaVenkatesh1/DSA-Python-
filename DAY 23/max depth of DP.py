0c719ad361d2ee67c8344f32310a7e92393d06e6
Deepika V <deepikavenkatesh109@gmail.com>
2026-04-10 19:47:18 +0530

    max depth of BT

---
+++ DAY 23/max depth of DP.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return 1 + max(
+            self.maxDepth(root.left),
+            self.maxDepth(root.right)
+        )
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+sol = Solution()
+print(sol.maxDepth(root))   # 3
